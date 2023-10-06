b6bc1ab58ff0ad892dd1511f4c61d0dc88b0c78e
Syed Murtaza <44249868+syedmurtaza108@users.noreply.github.com>
2023-10-06 11:42:32 +0000

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,3 +1,26 @@
+const authTokens = {
+  Admin: 'e220a1b1-cf45-4ccb-a557-ae9216cf231e',
+  'david.c@mailinator.com': '3cfab5ee-60ac-4745-98e1-a141fb8f7786',
+};
+
+const keys = Object.keys(authTokens);
+const select = document.createElement('select');
+
+select.addEventListener('change', function () {
+  window.ui.preauthorizeApiKey('authorization', select.value);
+});
+
+for (const element of keys) {
+  const option = document.createElement('option');
+  const text = document.createTextNode(element);
+  option.value = authTokens[element];
+  option.appendChild(text);
+  select.appendChild(option);
+}
+select.id = 'myDropdown';
+
 setTimeout(() => {
-    window.ui.preauthorizeApiKey('authorization', 'e220a1b1-cf45-4ccb-a557-ae9216cf231e');
-  }, 1000);
+  const descriptionDiv = document.querySelector('.description');
+  descriptionDiv.parentNode.replaceChild(select, descriptionDiv);
+  window.ui.preauthorizeApiKey('authorization', authTokens.Admin);
+}, 2000);
