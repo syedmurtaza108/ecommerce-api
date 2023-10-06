b9009587315df77dcc01d2602a3ffe29492f217a
Syed Murtaza <44249868+syedmurtaza108@users.noreply.github.com>
2023-10-06 15:17:49 +0500

    minor change

--- swagger.js
+++ swagger.js
@@ -1,1 +1,4 @@
-window.ui.preauthorizeApiKey('authorization', 'e220a1b1-cf45-4ccb-a557-ae9216cf231e');
+setTimeout(() => {
+    console.log(data);
+    window.ui.preauthorizeApiKey('authorization', 'e220a1b1-cf45-4ccb-a557-ae9216cf231e');
+  }, 1000);
